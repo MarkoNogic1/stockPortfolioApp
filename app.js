86798d1f86d577a558bc31ea3ede94fccb558f51
Marko Nogic <markonogic@wireless-b-1868.LaTech.edu>
2018-02-18 17:18:37 -0600

    Commiting template to use

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req,res) {
-    res.render("home.ejs");
+    res.render("home");
 })
 
 app.listen(3000, function () {
