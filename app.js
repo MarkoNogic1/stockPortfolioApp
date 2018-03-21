fb1a3f522aabdc98dd01e7a021a66ee72db3d16b
McKenzie Hafkemeyer <mckenzie.hafkemeyer@gmail.com>
2018-03-21 13:56:50 -0500

    Changes to restore routing after conversion to Node

--- app.js
+++ app.js
@@ -7,9 +7,28 @@ app.get("/", function(req,res){
     res.render("login.ejs");
 });
 
+app.get("/home", function(req,res){
+    res.render("home.ejs");
+});
+
 app.get("/editPortfolio", function(req,res){
     res.render("editPortfolio.ejs");
 });
+
+app.get("/viewStock", function(req,res){
+    res.render("viewStock.ejs");
+});
+
+app.get("/viewSector", function(req,res){
+    res.render("viewSector.ejs");
+});
+
+app.get("/register", function(req,res){
+    res.render("register.ejs");
+});
+
+
+
 
 app.listen(3000, function () {
     console.log('Stock application is listening on port 3000.');
