c00d7d8783afccb0267fd5297d04b946b5eb0577
Andy-Curtis <33043654+Andy-Curtis@users.noreply.github.com>
2018-03-25 21:30:57 -0500

    Updated to node.js version, and added email length check

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 var express = require("express");
 var app = express();
 
-app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", function (req,res) {
-    res.render("home");
-})
+app.get("/", function(req,res){
+    res.render("login.ejs");
+});
+
+app.get("/home", function(req,res){
+    res.render("home.ejs");
+});
+
+app.get("/editPortfolio", function(req,res){
+    res.render("editPortfolio.ejs");
+});
+
+app.get("/viewStock", function(req,res){
+    res.render("viewStock.ejs");
+});
+
+app.get("/viewSector", function(req,res){
+    res.render("viewSector.ejs");
+});
+
+app.get("/register", function(req,res){
+    res.render("register.ejs");
+});
+
+
+
 
 app.listen(3000, function () {
-    console.log("Server has started!")
+    console.log('Stock application is listening on port 3000.');
 });
