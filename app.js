e215b9cdf3f560af43dfe89255de36229a36306a
--global <dcr@wireless-c-005.LaTech.edu>
2018-03-26 14:16:18 -0500

    Added Nav Bar functionality to the Edit User Info page

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", function(req,res){
     res.render("login.ejs");
 });
 
+app.get("/home", function(req,res){
+    res.render("home.ejs");
+});
+
 app.get("/editPortfolio", function(req,res){
     res.render("editPortfolio.ejs");
 });
