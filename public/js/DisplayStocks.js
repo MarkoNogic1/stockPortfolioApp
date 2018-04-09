7a806091169582fc5f2d1a0a9c3101d1bcab8759
Danny Rogers <danny.rog@live.com>
2018-04-09 02:49:21 -0500

    Fixed nav bar, and made the displaystocks function an external javascript file

--- public/js/DisplayStocks.js
+++ public/js/DisplayStocks.js
@@ -1,21 +1,23 @@
 function myFunction() {
-
-    //What to do:
-    //XMLrequest for the database's string.
-    //Break it apart and call the function for each entry.
-    var xhttp = new XMLHttpRequest();
-    //What to do when the data is ready.
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200 && this.responseText !== undefined && this.responseText !== 'undefined' && this.responseText !== 'null' && this.responseText !== null && this.responseText !== '')
-        {
-            var longstring = this.responseText;
-		}
-	}
-	
-	
-    var para = document.createElement("P");
-    var t = document.createTextNode(longstring);
-    para.appendChild(t);
-    document.body.appendChild(para);
+  //What to do:
+  //XMLrequest for the database's string.
+  //Break it apart and call the function for each entry.
+  var xhttp = new XMLHttpRequest();
+  xhttp.open( "GET", "/getPortfolioData", false);
+  xhttp.send();
+  var answer = xhttp.responseText;
+  //What to do when the data is ready.
+  /*xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200 && this.responseText !== undefined && this.responseText !== 'undefined' && this.responseText !== 'null' && this.responseText !== null && this.responseText !== '')
+    {
+      var longstring = this.responseText;
+    }
+  }
+  */
+  var para = document.createElement("P");
+  var t = document.createTextNode(answer);
+  para.appendChild(t);
+  document.getElementsByClassName("col-lg-12")[0].appendChild(para);
 }
 
+myFunction();
