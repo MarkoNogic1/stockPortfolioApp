6ae2ab9ea445fff5595582bd562ba0adbf8cc1c3
Nicholas Reno Casteen <NickRCasteen@gmail.com>
2018-04-27 00:08:36 -0500

    Add files via upload

--- public/js/EditPortfolio_Functions.js
+++ public/js/EditPortfolio_Functions.js
@@ -7,7 +7,53 @@ function submit_ADD()
     requestStockData(stockname, TimeSeriesEnum.Batch).then(function(Dummy){
         if(Dummy.length > 152)
         {
-            document.getElementById('addStockForm').submit();
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.responseType = 'json';
+
+            xhttp.onreadystatechange = function()
+            {
+                var inputSector = document.getElementById("SectorNameList");
+                var sectornames = [];
+                //var duplicate = false;
+
+
+                if (this.readyState === 4 && this.status === 200)
+                {
+                    var jsonResponse = this.response;
+
+                    //Build list of sectors
+                    for(var i = 0; i<jsonResponse.length; ++i)
+                    {
+                        var sector = String(jsonResponse[i].sectorname);
+
+                        if(sectornames.includes(sector) === false)
+                        {
+                            sectornames.push(sector);
+                        }
+
+                    }
+
+                    //Check if exists
+                    for(i = 0; i<sectornames.length; ++i)
+                    {
+                        //alert(inputSector.value);
+                        if(sectornames[i].toUpperCase() === inputSector.value.toUpperCase())
+                        {
+                            inputSector.value = sectornames[i];
+                            break
+                        }
+                    }
+
+                    document.getElementById('addStockForm').submit();
+
+                }
+
+            };
+
+            xhttp.open("GET", "/getPortfolioData", true);
+            xhttp.send();
+
         }
         else
         {
@@ -41,6 +87,7 @@ function submit_EDIT()
     }
 }
 
+
 function addOptions()
 {
 
@@ -60,10 +107,16 @@ function addOptions()
     xhttp.onreadystatechange = function(){
         var select1 = document.getElementById("StockSelector1");
         var select2 = document.getElementById("StockSelector2");
+        var sectorSelect = document.getElementById("SectorName");
+        var sectorSelect2 = document.getElementById("EditSector");
+
+        var sectornames = [];
+
         if (this.readyState === 4 && this.status === 200)
         {
             var jsonResponse = this.response;
 
+            //GOING THROUGH JSON RESPONSE TO BUILD STOCK LIST AND BUILD ARRAY OF SECTOR NAMES
             for(var i = 0; i<jsonResponse.length; ++i)
             {
                 var opt = document.createElement('option');
@@ -77,23 +130,26 @@ function addOptions()
 
                 select1.appendChild(opt);
                 select2.appendChild(opt2);
+
+                var sector = String(jsonResponse[i].sectorname);
+                if(sectornames.includes(sector) === false)
+                {
+                    sectornames.push(sector);
+                }
             }
 
-                /*
-                if (StockName !== undefined && StockName !== 'undefined' && StockName !== 'null' && StockName !== null && StockName !== '')
-                {
-                    var opt = document.createElement('option');
-                    var opt2 = document.createElement('option');
+            //APPLYING ALL UNIQUE SECTOR NAMES
+            for(i = 0; i<sectornames.length; ++i)
+            {
+                var opt3 = document.createElement('option');
+                var opt4 = document.createElement('option');
 
-                    opt.value = StockName;
-                    opt.innerHTML = StockName;
-                    opt2.value = StockName;
-                    opt2.innerHTML = StockName;
+                opt3.value = sectornames[i];
+                opt4.value = sectornames[i];
 
-                    select1.appendChild(opt);
-                    select2.appendChild(opt2);
-                }*/
-
+                sectorSelect.appendChild(opt3);
+                sectorSelect2.appendChild(opt4);
+            }
 
         }
 
